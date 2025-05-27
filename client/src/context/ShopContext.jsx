c8499e0703d9ee8334ac1aee743d874ecbd9a920
agwebryanmuna <agwebryan1@gmail.com>
2025-05-27 12:16:22 +0100

    complete searchbar component

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/assets";
 
 export const ShopContext = createContext(null);
@@ -6,8 +6,18 @@ export const ShopContext = createContext(null);
 const ShopContextProvider = ({ children }) => {
   const currency = "$";
   const deliveryFee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
 
-  const value = { products, currency, deliveryFee };
+  const value = {
+    products,
+    currency,
+    deliveryFee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
+  };
 
   return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
 };
