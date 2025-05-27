a6c5b6a3decfb7c0e10809f50fe41e0ea0dbd23a
agwebryanmuna <agwebryan1@gmail.com>
2025-05-27 01:00:59 +0100

    Created bestseller and latestcollection sections

---
+++ client/src/context/ShopContext.jsx
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+import { products } from "../assets/assets";
+
+export const ShopContext = createContext(null);
+
+const ShopContextProvider = ({ children }) => {
+  const currency = "$";
+  const deliveryFee = 10;
+
+  const value = { products, currency, deliveryFee };
+
+  return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
+};
+
+export default ShopContextProvider;
