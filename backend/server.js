27b27fdb427757ff14d8fa7fe8626b7451407788
agwebryanmuna <agwebryan1@gmail.com>
2025-05-31 04:02:30 +0100

    Configure mongodb/mongoose and cloudinary

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,25 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { connectDb } from "./config/mongodb.js";
+import { connecCloudinary } from "./config/cloudinary.js";
 
 // App config
 const app = express();
+const port = process.env.PORT || 4000;
+connectDb();
+connecCloudinary();
+
+// middlewares
+app.use(express.json()); // parse body of requests
+app.use(cors()); // Access backend from any ip/domain
+
+// api endpoints
+
+app.get("/", (req, res) => {
+  res.send("API working 😊");
+});
+
+app.listen(port, () =>
+  console.log(`Server running on http://localhost:${port}`)
+);
