46d77a873b978572d869a3510373b93f1e651826
agwebryanmuna <agwebryan1@gmail.com>
2025-05-31 04:02:30 +0100

    Configure mongodb/mongoose and cloudinary

---
+++ backend/config/mongodb.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB connection established 🎉");
+  } catch (err) {
+    console.log("MongoDB connection failed 😢", err);
+  }
+};
