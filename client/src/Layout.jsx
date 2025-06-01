b426193e3193bb9e645b394484a00f789c9c8af6
agwebryanmuna <agwebryan1@gmail.com>
2025-06-01 12:34:09 +0100

    Setup admin routes

---
+++ client/src/Layout.jsx
@@ -0,0 +1,35 @@
+import { Route, Routes } from "react-router";
+import App, { Dashboard } from "./App";
+import Add from "./pages/admin/Add";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
+
+const Layout = () => {
+  return (
+    <Routes>
+      <Route path="/admin" element={<Dashboard />}>
+        <Route index element={<Add />} />
+      </Route>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default Layout;
