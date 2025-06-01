d1132e157334ed413ff6067d328b127d4d05f3a2
agwebryanmuna <agwebryan1@gmail.com>
2025-06-01 11:36:05 +0100

    created admin auth

--- backend/controllers/userContrller.js
+++ backend/controllers/userContrller.js
@@ -82,6 +82,26 @@ const registerUser = async (req, res) => {
 };
 
 // -------- Admin login
-const adminLogin = async (req, res) => {};
+const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password)
+      return res.json({ success: false, message: "All fields are required." });
+
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, error: error.message });
+  }
+};
 
 export { loginUser, registerUser, adminLogin };
