00ae633d349098004fc79dadd906edaf102420bf
agwebryanmuna <agwebryan1@gmail.com>
2025-06-01 12:34:09 +0100

    Setup admin routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,32 @@
-import { Routes, Route } from "react-router";
-import Home from "./pages/Home";
-import Collection from "./pages/Collection";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Product from "./pages/Product";
-import Cart from "./pages/Cart";
-import Login from "./pages/Login";
-import PlaceOrder from "./pages/PlaceOrder";
-import Orders from "./pages/Orders";
+import { Outlet } from "react-router";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Searchbar from "./components/Searchbar";
 import { ToastContainer } from "react-toastify";
+import ShopContextProvider from "./context/ShopContext";
+
+export const Dashboard = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      {/* <AdminNavbar/> */}
+      <h1>Hello Administrator</h1>
+      <Outlet />
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-      <ToastContainer />
-      <Navbar />
-      <Searchbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/collection" element={<Collection />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/product/:productId" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/place-order" element={<PlaceOrder />} />
-        <Route path="/orders" element={<Orders />} />
-      </Routes>
-      <Footer />
-    </div>
+    <ShopContextProvider>
+      <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+        <ToastContainer />
+        <Navbar />
+        <Searchbar />
+        <Outlet />
+        <Footer />
+      </div>
+    </ShopContextProvider>
   );
 };
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
-import ShopContextProvider from "./context/ShopContext.jsx";
+import Layout from "./Layout.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <ShopContextProvider>
-      <App />
-    </ShopContextProvider>
+    <Layout />
   </BrowserRouter>
 );
