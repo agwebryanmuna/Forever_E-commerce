4245a8dbf08aa008f78b5aa2c346956ba1af9760
agwebryanmuna <agwebryan1@gmail.com>
2025-07-19 05:14:58 +0100

    Still trying to fix vercel 404

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/',
+  baseURL: '/',
   plugins: [react(), tailwindcss()],
 });
